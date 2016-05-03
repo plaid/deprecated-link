ec398f8e9f334a79b0bcf0bcf442d1b52a0d8e6b
Paolo Bernasconi <paolo@plaid.com>
2016-05-03 15:40:22 -0700

    Add RequireJS example

---
+++ examples/requirejs/public/scripts/config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// configure RequireJS to load the link-initialize script
+// *NOTE: the '*.js' extension is ommittedg
+requirejs.config({
+  paths: {
+    'Plaid': 'https://cdn.plaid.com/link/stable/link-initialize'
+  },
+  // Plaid Link doesn't current support AMD
+  // therefore we must create a shim to expose the global handler
+  shim: {
+    'Plaid': {
+      exports: 'window.Plaid'
+    }
+  }
+});
