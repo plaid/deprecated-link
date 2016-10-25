5f958668ed5f81861fee65d515f60894765c9481
Paolo Bernasconi <pbernasconi@users.noreply.github.com>
2016-10-25 09:17:13 -0700

    Add Mobile Beta release and examples (#111)
    * Adding mobile beta examples
    
    * relative links

---
+++ mobile-beta/uiwebview/JavaScript WebView/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  PlaidLinkWebviewExample
+//
+//  Copyright (c) 2016 Plaid Technologies, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
